a574b739be4d992ddf7a63f61d4ad8d4dd888033
incredible92 <sulymanabdullahi@gmail.com>
2020-12-10 16:00:18 +0100

    feat: todo

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -3,28 +3,30 @@ function Todo() {
     <div className="Todo">
       <h2 className="todo">Todo</h2>
 
-      <div className="input1">
+      <div className="input1 field">
+        {" "}
         <input
-          className="input-field"
+          className="input-field "
           type="text"
           name="text"
           placeholder="buy some milk"
-        />
+        />{" "}
+        <img className="bin" src="\image\Vector.png" alt="bin" />
       </div>
-
       <div className="input1">
         <input
           className="input-field"
           type="text"
           name="text"
           placeholder="cook some food"
-        />
+        />{" "}
+        <img className="bin" src="\image\Vector.png" alt="bin" />
       </div>
       <div className="add"> Add a new todo:</div>
 
       <div className="input">
         <input
-          className="input-field"
+          className="task"
           type="text"
           name="text"
           placeholder="Enter task and press enter"
